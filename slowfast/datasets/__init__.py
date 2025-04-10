1e30d6308c596b18306e062a54a9bebc29655dde
wuallenwu <allenwu@andrew.cmu.edu>
2025-04-10 15:50:53 -0400

    update yaml, make sure remag actually being read

--- slowfast/datasets/__init__.py
+++ slowfast/datasets/__init__.py
@@ -7,6 +7,7 @@ from .charades import Charades  # noqa
 from .imagenet import Imagenet  # noqa
 from .kinetics import Kinetics  # noqa
 from .ssv2 import Ssv2  # noqa
+from .remag_dataset import REMAGDataset  # noqa
 
 try:
     from .ptv_datasets import Ptvcharades, Ptvkinetics, Ptvssv2  # noqa
